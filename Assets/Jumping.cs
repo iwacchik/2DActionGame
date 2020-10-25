039588ad99b3516e989ee49f8dc12559a6c20d05
iwacchik <iwacchi.kz@gmail.com>
2020-10-25 20:17:06 +0900

    現在の最新をコミット

---
+++ Assets/Jumping.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+
+    public bool isGUI = true;
+
+    public Rigidbody jumpObject = null;
+    readonly float jumpForce = 390.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnGUI ()
+	{
+        if (isGUI)
+        {
+            //GUI.Box(new Rect(Screen.width - 110, 10, 100, 90), "Jump Motion");
+            if (GUI.Button(new Rect(Screen.width - 100, Screen.height - 40, 80, 20), "Jump"))
+                if( jumpObject && jumpObject.velocity.y == 0f ) jumpObject.AddForce( transform.up * this.jumpForce );
+        }
+	}
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	private CharacterController characterController;
+	private Vector3 velocity;
+	[SerializeField]
+	private float walkSpeed = 0;
+
+    private Animator animator;
+    private AnimatorStateInfo currentState;		// 現在のステート状態を保存する参照
+	private AnimatorStateInfo previousState;	// ひとつ前のステート状態を保存する参照
+
+	[SerializeField]
+	private float jumpPower = 0;
+	private float currentJumpPower = 0;
+
+
+	bool canMove {
+		get{
+			var state = animator.GetCurrentAnimatorStateInfo(0);
+
+			return state.fullPathHash == Animator.StringToHash("Base Layer.Standing@loop")
+			    || state.fullPathHash == Animator.StringToHash("Base Layer.Walking@loop")
+			    || state.fullPathHash == Animator.StringToHash("Base Layer.Running@loop");
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		characterController = GetComponent <CharacterController> ();
+        animator = GetComponent <Animator> ();
+        currentState = animator.GetCurrentAnimatorStateInfo (0);
+	}
+	
+	// Update is called once per frame
+    void Update () {
+		if(characterController.isGrounded ) {
+			currentJumpPower = 0f;
+			if( canMove ){
+				velocity = new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
+ 
+				if(velocity.magnitude > 0.01f){
+					animator.SetFloat("Speed", velocity.magnitude);
+					transform.LookAt(transform.position + velocity);
+				} else {
+					animator.SetFloat("Speed", 0f);
+				}
+
+				velocity *= walkSpeed;
+
+				if (Input.GetKeyDown(KeyCode.Space))//  もし、スペースキーがおされたら、
+            	{
+                	currentJumpPower = jumpPower;//  y座標をジャンプ力の分だけ動かす
+					animator.SetTrigger( "JumpTrigger" );
+            	}
+			}
+		}
+		else if( animator.GetCurrentAnimatorStateInfo(0).fullPathHash == Animator.StringToHash("Base Layer.OnGround") ){
+			velocity.x = 0f;
+			velocity.z = 0f;
+		}
+
+		//Debug.Log( animator.GetCurrentAnimatorStateInfo(0).fullPathHash != Animator.StringToHash("Base Layer.OnGround") );
+		// Debug.Log( animator.GetCurrentAnimatorStateInfo(0).fullPathHash );
+
+		currentJumpPower += Physics.gravity.y * Time.deltaTime;
+		velocity.y = currentJumpPower;
+		characterController.Move(velocity * Time.deltaTime);
+
+		animator.SetBool("OnGround", characterController.isGrounded );
+	}
+}
